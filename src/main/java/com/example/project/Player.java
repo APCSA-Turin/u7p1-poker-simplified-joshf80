30f61fa8437d3b586653fc0c2d29e69621a770d8
Joshua <139407249+joshf80@users.noreply.github.com>
2025-03-03 09:05:40 -0500

    Update Player.java

--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -23,16 +23,49 @@ public class Player{
         return "Nothing";
     }
 
-    public void sortAllCards(){} 
+     public void SortCards(){
+        for (int i = 1; i < allCards.size(); i++) {
+            int k = Utility.getRankValue(allCards.get(i).getRank());
+            int j = i - 1;
+   
+            while (j >= 0 && Utility.getRankValue(allCards.get(j).getRank()) > k) {
+                allCards.set(j + 1, allCards.get(j));
+                j--;
+            }
+           
+            allCards.set(j + 1, allCards.get(k));
+        }
+    } 
 
     public ArrayList<Integer> findRankingFrequency(){
-        return new ArrayList<>(); 
+        ArrayList<Integer> freqList = new ArrayList<Integer>();
+        for (String string : Utility.getRanks()) {
+            int count = 0;
+            for (int i = 0; i < allCards.size(); i++) {
+                if (Utility.getRankValue(allCards.get(i).getRank()) == Integer.parseInt(string)) {
+                    count += 1;
+                }
+            }
+            freqList.add(count);
+        }
+
+        return freqList; 
     }
 
     public ArrayList<Integer> findSuitFrequency(){
-        return new ArrayList<>(); 
-    }
+        ArrayList<Integer> freqList = new ArrayList<Integer>();
+        for (String string : Utility.getSuits()) {
+            int count = 0;
+            for (int i = 0; i < allCards.size(); i++) {
+                if (allCards.get(i).getSuit().equals(string)) {
+                    count += 1;
+                }
+            }
+            freqList.add(count);
+        }      
 
+        return freqList; 
+    }
    
     @Override
     public String toString(){
